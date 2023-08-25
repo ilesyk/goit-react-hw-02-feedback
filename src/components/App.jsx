454cb7820b402088324fc08f3a130642471eebf7
ilesyk <pavloyavorskyi@ukr.net>
2023-08-25 10:17:41 +0300

    feedback done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,59 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React, { Component } from "react";
+import { Feedback } from "./Feedback"
+import { Message } from "./Message";
+import { Section } from "./Section";
+import { Statistics } from "./Statistics";
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  updateState = option => {
+    this.setState(prevState => {
+      return {
+        [option]: prevState[option] + 1,
+      };
+    });
+  };
+  countTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    return Math.round(
+      (this.state.good /
+        (this.state.good + this.state.neutral + this.state.bad)) *
+        100
+    );
+  };
+
+  render() {
+    return (
+      <div>
+        <Section title='Please leave feedback'>
+          <Feedback
+            options={Object.keys(this.state)}
+            onLeaveFeedback={this.updateState}
+          />
+        </Section>
+        <Section title='No feedback given'>
+          {this.countTotalFeedback() === 0 ? (
+            <Message message='No feedback given' />
+          ) : (
+            <Statistics
+              good={this.state.good}
+              neutral={this.state.neutral}
+              bad={this.state.bad}
+              feedbacks={this.state}
+              total={this.countTotalFeedback()}
+              percentage={this.countPositiveFeedbackPercentage()}
+            />
+          )}
+        </Section>
+      </div>
+    );
+  }
 };
